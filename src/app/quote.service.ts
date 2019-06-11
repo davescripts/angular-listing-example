d5be5a288b25399ce083944640cb1fcf6853bf25
davescripts <dave@davescripts.com>
2019-06-10 22:27:52 -0700

    Add Http to the QuoteService.

--- src/app/quote.service.ts
+++ src/app/quote.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
 
+import { Quote } from './quote';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
 export class QuoteService {
 
-  constructor() { }
+  private quotesUrl = 'https://raw.githubusercontent.com/davescripts/test-data/master/quotes.json';
+
+  constructor(private http: HttpClient) { }
+
+  getQuotes(): Observable<Quote[]> {
+    return this.http.get<Quote[]>(this.quotesUrl)
+      .pipe(
+        catchError(this.handleError<Quote[]>('getQuotes', []))
+      );
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error);
+      return of(result as T);
+    };
+  }
+
 }
